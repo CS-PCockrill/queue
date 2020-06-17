45d0c3ae22574909eb1519e8525259b4e2a3a58e
Babatunde Falade <tunfal1@hotmail.com>
2020-06-17 19:45:58 -0400

    added helper file

---
+++ cmd/appCode/helper.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/CS-PCockrill/queue/pkg/models"
+
+func (a *appInjection) isAuthenticated(user models.User) bool {
+	// Authenticated whether the user is logged in
+	// Return T or F
+	return false
+}
